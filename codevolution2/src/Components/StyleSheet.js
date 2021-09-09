519269159ae5cfbc5795876687c158ec5533be1c
Biswajit <biswajit.majumdar@carnationit.com>
2021-09-09 14:17:38 +0530

    first commit lecture 20 comtinue

---
+++ codevolution2/src/Components/StyleSheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './myStyle.css'
+
+function StyleSheet() {
+    return (
+        <div>
+            <h1 className = 'primary'> Style </h1>
+        </div>
+    )
+}
+
+export default StyleSheet
---
+++ codevolution2/src/Tutorial20.js
@@ -0,0 +1,17 @@
+/*   -----Index and Key Anti-Pattern ----- */
+
+import './App.css';
+import StyleSheet from './Components/StyleSheet'; 
+
+
+
+function App() {
+    return (
+      <div className="App">
+          <StyleSheet/>
+     
+      </div>
+    );
+  }
+
+export default App;
