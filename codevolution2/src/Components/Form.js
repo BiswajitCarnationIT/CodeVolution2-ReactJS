956884888f18dc2d8f0358d92adeab23860032fe
Biswajit <biswajit.majumdar@carnationit.com>
2021-09-10 15:30:59 +0530

    Tutorial 21 complete form Handling onChange

--- codevolution2/src/Components/Form.js
+++ codevolution2/src/Components/Form.js
@@ -6,20 +6,55 @@ class Form extends Component {
         super(props)
     
         this.state = {
-             username : ''
+             username : '',
+            comments: '',
+            topic:'React'
         }
     }
 
     handlerUsernameChange = (eventUsername) => {
         this.setState({username : eventUsername.target.value})
     }
+    handelCommentChange = (event) => {
+        this.setState({comments: event.target.value})
+    }
+    handelTopicChange = event => {
+        this.setState({topic: event.target.value})
+    }
+
+    handelSubmit = event =>{
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+        event.preventDefault()  //page does not refress aafter submission
+    }
     
     render() {
+        const {username,comments,topic} = this.state // destructuring
         return (
-            <form>
-                <lable> User Name </lable>
-                <input type = 'text' 
-                value = {this.state.username} onChange = {this.handlerUsernameChange} />
+            <form onSubmit = {this.handelSubmit}>
+                <div>
+                    <lable> User Name </lable>
+                    <input type = 'text' 
+                    value = {this.state.username} onChange = {this.handlerUsernameChange} />
+                </div>
+
+                <div>
+                    <lable> Comments </lable>
+                    <textarea value = {this.state.comments}
+                    onChange = {this.handelCommentChange}></textarea>
+                </div>
+
+                <div>
+                    <label>Topic</label>
+                    <select value = {this.state.topic} onChange = {this.handelTopicChange}>
+                        <option value = "React">React</option>
+                        <option value = "Angular">Angular</option>
+                        <option value = "Vue">Vue</option>
+                    </select>
+                </div>
+                <button type = "submit">Submit</button>
+                
+
+
             </form>
         )
     }
