48fe11abc531f3b72370a07edcb88152a7796811
Biswajit <biswajit.majumdar@carnationit.com>
2021-09-13 10:46:08 +0530

    Lecture 25 Fragments complete

--- codevolution2/src/Components/Columns.js
+++ codevolution2/src/Components/Columns.js
@@ -1,12 +1,33 @@
+import react from 'react'
 import React from 'react'
 import { Fragment } from 'react'
 
 function Columns() {
+    const item = []
+    /*
     return (
         <React.Fragment>
+            {
+                item.map( item => (
+                    <React.Fragment key = {item.id}>
+                        <h1>Title</h1>
+                        <p>{item.title}</p>
+                    </React.Fragment>
+                ))
+            }
             <td>Name</td>
             <td>Biswajit</td>
         </React.Fragment>
+        
+    ) 
+    */
+    return (
+        
+        <>
+            
+            <td>Name</td>
+            <td>Biswajit</td>
+        </>
     )
 }
 
